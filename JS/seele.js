9fac5d332f2f6bee4c2d1f6c84a4b843781f81e8
ImeneeSh <nourelimene.sahi@se.univ-bejaia.dz>
2025-03-03 13:50:18 +0100

    j'ai changé des trucs pour tester

--- JS/seele.js
+++ JS/seele.js
@@ -7,6 +7,7 @@ particlesJS("particles-js", {
             stroke: {width: 0, color: "#000000"},
             polygon: {nb_sides: 5},
             image: {src: "img/github.svg", width: 100, height: 100}
+
         },
         opacity: {
             value: 0.5,
