2cfca464050ad84fddb2e081b1eaa150a2a597f8
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-04-10 20:07:43 +0100

    Define url as a const

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {stringifyQuery} from "vue-router";
 
 export default defineConfig({
     plugins: [vue()],
     server: {
         port: 5179,
         open: true,
+    },
+    define: {
+        apiurl: JSON.stringify('https://crystal.tilde.institute/mellow/')
     }
 })
