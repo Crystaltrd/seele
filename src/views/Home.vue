efa129222927be689e95439c3a465798e79f9a1c
ImeneeSh <nourelimene.sahi@se.univ-bejaia.dz>
2025-04-12 17:51:18 +0100

    changed loading animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,7 +19,10 @@
                 </div>
               </div>
             </template>
-            <p v-else style="color: white">Loading Books</p>
+
+            <svg viewBox="25 25 50 50" v-else >
+              <circle r="20" cy="50" cx="50"></circle>
+            </svg>
           </div>
         </div>
       </div>
@@ -208,6 +211,45 @@ export default defineComponent({
   font-weight: 500;
   text-align: center;
 }
+
+svg {
+  width: 3.25em;
+  transform-origin: center;
+  animation: rotate4 2s linear infinite;
+}
+
+circle {
+  fill: none;
+  stroke: hsl(214, 97%, 59%);
+  stroke-width: 2;
+  stroke-dasharray: 1, 200;
+  stroke-dashoffset: 0;
+  stroke-linecap: round;
+  animation: dash4 1.5s ease-in-out infinite;
+}
+
+@keyframes rotate4 {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes dash4 {
+  0% {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+  }
+
+  50% {
+    stroke-dasharray: 90, 200;
+    stroke-dashoffset: -35px;
+  }
+
+  100% {
+    stroke-dashoffset: -125px;
+  }
+}
+
 
 @media (max-width: 768px) {
   .section-heading {
