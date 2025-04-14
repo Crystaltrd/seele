05b0dc71b830b51246362b85f9d49deffa5ba663
ImeneeSh <nourelimene.sahi@se.univ-bejaia.dz>
2025-04-14 21:57:58 +0100

    added a light border

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -118,7 +118,8 @@ export default defineComponent({
   border-radius: 20px;
   padding: 0 1.5rem;
   width: 1100px;
-  box-shadow: 0 8px 32px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 8px rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.5);
 }
 
 .section-heading {
